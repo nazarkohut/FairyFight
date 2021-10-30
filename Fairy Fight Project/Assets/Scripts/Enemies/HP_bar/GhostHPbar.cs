ddcc70f0fef6cf6832cb13ca773c543364ef1ee4
Nazarii Hryhorash <grigorashnazar@gmail.com>
2021-10-30 19:48:07 +0300

    HP_bar has been added

---
+++ Fairy Fight Project/Assets/Scripts/Enemies/HP_bar/GhostHPbar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostHPbar : MonoBehaviour
+{
+    Vector3 localScale;
+    public Ghost ghost;
+    Vector3 startLocalScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        startLocalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(ghost.HealthPoint);
+        localScale.x = ((float)ghost.HealthPoint / (float)ghost.MaxHealthPoint) * (float)startLocalScale.x;
+        transform.localScale = localScale;
+    }
+}
