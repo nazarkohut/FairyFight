90136aa962fb7210e01af8d88f7b721bca4efe10
VasyaYovbak <vasa23102003@gmail.com>
2021-10-30 14:10:49 +0300

    added damage sound effect

--- Fairy Fight Project/Assets/Scripts/Player/Bullet.cs
+++ Fairy Fight Project/Assets/Scripts/Player/Bullet.cs
@@ -8,16 +8,20 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     public float speed;
 
-    Vector3 way;
+    public  Vector3 way;
     void Start()
     {
-        if (Fairy.isRight){
-            way = new Vector3(1, 0, 0);
-        }else
+        if (Fairy.isRight)
+        {
+           way = new Vector3(1, 0, 0);
+        }
+        else
             way = new Vector3(-1, 0, 0);
     }
+    
     private void Update()
     {   
+
         gameObject.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(speed, 0,0);    
         gameObject.GetComponent<Rigidbody2D>().transform.right = way;
     }
